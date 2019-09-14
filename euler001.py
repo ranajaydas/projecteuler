ca4cb8d1e10d0503bfc35903114711ad8e2b893a
ranajaydas <ranajay@outlook.com>
2019-09-14 16:44:44 +0300

    Sum of multiples of 3 or 5 below 1000.

---
+++ euler001.py
@@ -0,0 +1,22 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+https://projecteuler.net/problem=1
+"""
+
+
+def sum_mul_below(listofmuls: list, n: int) ->int:
+    """Returns sum of all the multiples of 'listofmuls' below 'n'."""
+    sum = 0
+    for i in range(n):
+        for mul in listofmuls:
+            if i % mul == 0:
+                sum += i
+                break
+    return sum
+
+
+if __name__ == '__main__':
+    print(sum_mul_below(listofmuls=[3, 5], n=1000))
