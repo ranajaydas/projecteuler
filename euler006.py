43e110a2e3301798c3ca1e74b75cecf36f1de455
ranajaydas <ranajay@outlook.com>
2019-09-15 15:30:45 +0300

    Difference between sum of squares and square of sums to 100.

---
+++ euler006.py
@@ -0,0 +1,30 @@
+"""
+The sum of the squares of the first ten natural numbers is 385
+The square of the sum of the first ten natural numbers is 3025
+Their difference is 2640
+
+Find the difference between the sum of the squares of
+the first one hundred natural numbers and the square of the sum
+
+https://projecteuler.net/problem=6
+"""
+
+
+def sum_of_square_to(n: int) -> int:
+    sum = 0
+    for i in range(1, n+1):
+        sum += i ** 2
+    return sum
+
+
+def square_of_sum_to(n: int) -> int:
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum ** 2
+
+
+if __name__ == '__main__':
+    n = 100
+    print(square_of_sum_to(n) - sum_of_square_to(n))
+
